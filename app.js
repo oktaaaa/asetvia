64a47498209fe5f182fd83d6b1af1f13a20c0bf4
Okta P <57696739+oktaaaa@users.noreply.github.com>
2024-10-20 08:46:05 +0700

    cors

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+
+// ADD THIS
+var cors = require('cors');
+app.use(cors());
 require('dotenv/config')
 
 
