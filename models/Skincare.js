c23f3c2aaa54d6560bd356b884b804a97e21e57f
Okta P <57696739+oktaaaa@users.noreply.github.com>
2025-07-03 16:44:00 +0700

    cat loc

--- models/Skincare.js
+++ models/Skincare.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const SkincareSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    },
-    expiredYear: {
-        type: Date,
-        required: true
-    },
-    stock:  {
-        type: Number,
-        required: true
-    },
-    
-    position: {
-        type: String,
-        required: true
-    },
-    review:{
-        type: String,
-        required: true
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  expiredYear: {
+    type: Date,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Category",
+  },
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Location",
+  },
+  review: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('Skincare', SkincareSchema)
+module.exports = mongoose.model("Skincare", SkincareSchema);
